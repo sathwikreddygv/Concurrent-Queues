4ae559fec7980113571a9c1cc8e83d6219ddae90
sathwikreddygv <49616121+sathwikreddygv@users.noreply.github.com>
2020-12-23 19:08:22 +0530

    Add files via upload

--- Basket_queue.cpp
+++ Basket_queue.cpp
@@ -8,7 +8,7 @@
 #include <atomic>
 using namespace std;
 #define MAX_HOPS 3
-
+int n,k;
 default_random_engine generator;
 float l1,l2;
 
@@ -37,6 +37,7 @@ public:
 		tag = t;
 	}
 };
+
 
 class node_t
 {
@@ -77,16 +78,16 @@ public:
 		head.store(temp);
 	}
 
-	void enqueue(queue_t* q, int val)
+	void enqueue(int val)
 	{
 		node_t* nd = new node_t();
         nd->value = val;
 		pointer_t check_tail, check_next;
         while(1)
         {
-        	check_tail = q->tail.load();  //full confusemnet. ok write
+        	check_tail = tail.load();  //full confusemnet. ok write
         	check_next = check_tail.ptr->next.load();
-			if(comparePointer(check_tail,q->tail.load()))
+			if(comparePointer(check_tail,tail.load()))
 			{
 				if(check_next.ptr==NULL)
 				{
@@ -99,7 +100,7 @@ public:
 					// temp.ptr=nd;
 					// temp.deleted = 0;
 					// temp.tag=check_tail.tag+1;
-					if((q->tail.load().ptr->next).compare_exchange_strong(check_next, temp))
+					if((tail.load().ptr->next).compare_exchange_strong(check_next, temp))
 					{
 						//CAS
 						pointer_t temp2(nd,0,check_tail.tag+1);
@@ -107,19 +108,19 @@ public:
 						// temp2.ptr=nd;
 						// temp2.deleted = 0;
 						// temp2.tag=check_tail.tag+1;
-						(q->tail).compare_exchange_strong(check_tail, temp2);
+						(tail).compare_exchange_strong(check_tail, temp2);
 						return;
 					}
 					check_next = check_tail.ptr->next.load();
 					while((check_next.tag==check_tail.tag+1)&&!(check_next.deleted))
 					{
 						//backoff
-						exponential_distribution<double> distribution_one(l1);
-					    auto t1 = distribution_one(generator);
-					    sleep((int)(t1));
+						// exponential_distribution<double> distribution_one(l1);
+					    // auto t1 = distribution_one(generator);
+					    // sleep((int)(t1));
 						// nd->next = check_next;
 						nd->next.store(check_next);
-						if((q->tail.load().ptr->next).compare_exchange_strong(check_next, temp))
+						if((tail.load().ptr->next).compare_exchange_strong(check_next, temp))
 						{
 							return;
 						}
@@ -128,7 +129,7 @@ public:
 				}
 				else
 				{
-					while((check_next.ptr->next.load().ptr != NULL)&&comparePointer(q->tail.load(),check_tail))
+					while((check_next.ptr->next.load().ptr != NULL)&&comparePointer(tail.load(),check_tail))
 					{
 						check_next = check_next.ptr->next.load();
 					}
@@ -138,7 +139,7 @@ public:
 					// temp1.ptr=check_next.ptr;
 					// temp1.deleted = 0;
 					// temp1.tag=check_tail.tag+1;
-					q->tail.compare_exchange_strong(check_tail, temp1);
+					tail.compare_exchange_strong(check_tail, temp1);
 				}
 				
 			}
@@ -146,22 +147,22 @@ public:
         }
 	}
 
-    int dequeue(queue_t* q,int i)
+    int dequeue()
     {
         while(1)
         {
-            pointer_t check_head = q->head.load();
-            pointer_t check_tail = q->tail.load();
+            pointer_t check_head = head.load();
+            pointer_t check_tail = tail.load();
             pointer_t check_next = check_head.ptr->next.load();
 
-            if(comparePointer(check_head, q->head.load()))
+            if(comparePointer(check_head, head.load()))
             {
                 if(check_head.ptr == check_tail.ptr)
                 {
                     if(check_next.ptr == NULL)
                         return -1;
 
-                    while((check_next.ptr->next.load().ptr != NULL) && comparePointer(q->tail.load(),check_tail))
+                    while((check_next.ptr->next.load().ptr != NULL) && comparePointer(tail.load(),check_tail))
                     {
                         check_next = check_next.ptr->next.load();
                     }
@@ -170,23 +171,23 @@ public:
 					// temp1.ptr=check_next.ptr;
 					// temp1.deleted = 0;
 					// temp1.tag=check_tail.tag+1;
-					(q->tail).compare_exchange_strong(check_tail, temp1);
+					(tail).compare_exchange_strong(check_tail, temp1);
                 }
                 else
                 {
                     pointer_t iter = head;
                     int hops = 0;
 
-                    while((check_next.deleted && iter.ptr != check_tail.ptr) && comparePointer(q->head.load(), check_head))
+                    while((check_next.deleted && iter.ptr != check_tail.ptr) && comparePointer(head.load(), check_head))
                     {
                         iter = check_next;
                         check_next = iter.ptr->next.load();
                         hops++;
                     }
-                    if(!comparePointer(q->head.load(),check_head)) 
+                    if(!comparePointer(head.load(),check_head)) 
                         continue;
                     else if(iter.ptr == check_tail.ptr)
-                        free_chain(q, check_head, iter);
+                        free_chain(this, check_head, iter);
                     else
                     {
                         int value = check_next.ptr->value;
@@ -199,17 +200,15 @@ public:
                         if(iter.ptr->next.compare_exchange_strong(check_next,temp))//CAS
                         {
                             if(hops >= MAX_HOPS)
-                                free_chain(q, check_head, check_next);
+                                free_chain(this, check_head, check_next);
                             return value;
                         }
                        // backoff-scheme();
-					   exponential_distribution<double> distribution_one(l2);
-					   auto t1 = distribution_one(generator);
-					   sleep((int)(t1));
+					//    exponential_distribution<double> distribution_one(l2);
+					//    auto t1 = distribution_one(generator);
+					//    sleep((int)(t1));
                     }
                 }
-                
-                
             }
         }
     }
@@ -231,39 +230,122 @@ void free_chain(queue_t *q,pointer_t check_head,pointer_t new_head)
 	}
 }
 
-queue_t opt_q;
+queue_t bs_q;
 
-void test(int i){
-    if(i<7){
-        opt_q.enqueue(&opt_q, i+1);
-        auto end_time = chrono::system_clock::to_time_t(chrono::system_clock::now());
-        string t = ctime(&end_time);
-        t = t.substr(10,9);
-        cout<<"enqed "<<i+1<<" at "<<t<<endl;
-    }
-    else{
-        int ret = opt_q.dequeue(&opt_q,i);
-        auto end_time = chrono::system_clock::to_time_t(chrono::system_clock::now());
-        string t = ctime(&end_time);
-        t = t.substr(10,9);
-        cout<<"deqed "<<i+1<<" at "<<t<<endl;
-    }
+void test_random(int id){
+	for(int i=0;i<k;i++){
+		//srand(time(0));
+		if(rand()&1){
+            int enq = rand()%100;
+            bs_q.enqueue(enq);
+            printf("thread %d enqueued %d\n",id+1,enq );
+            /*****************************************************
+             comment above line and uncomment below lines
+             to get time stamps. This could give outofrange error
+             which we could not rectify, but it does not have anything
+             to do with the working of the queue
+            ******************************************************/
+            // time_t end_time = chrono::system_clock::to_time_t(chrono::system_clock::now());
+		    // string t = ctime(&end_time);
+		    // t = t.substr(10,9);
+            // printf("thread %d enqueued %d at %s\n",id+1,enq,t.c_str() );
+		}
+		else{
+			int ret = bs_q.dequeue();
+            printf("thread %d dequeued %d\n", id+1, ret);
+            /*****************************************************
+             comment above line and uncomment below lines
+             to get time stamps. This could give outofrange error
+             which we could not rectify, but it does not have anything
+             to do with the working of the queue
+            ******************************************************/
+            // time_t end_time = chrono::system_clock::to_time_t(chrono::system_clock::now());
+            // string t = ctime(&end_time);
+            // t = t.substr(10,9);
+            // printf("thread %d dequeued %d at %s\n", id+1, ret, t.c_str());
+		}
+	}
 }
 
-int main()
-{
-	l1=1.0/10;
-	l2=1.0/10;
-	int n = 10;
+void test_alternate(int id){
+	for(int i=0;i<k;i++){
+		if(i&1){
+            int enq = rand()%100;
+			bs_q.enqueue(enq);
+            printf("thread %d enqueued %d\n",id+1,enq );
+            /*****************************************************
+             comment above line and uncomment below lines
+             to get time stamps. This could give outofrange error
+             which we could not rectify, but it does not have anything
+             to do with the working of the queue
+            ******************************************************/
+            // time_t end_time = chrono::system_clock::to_time_t(chrono::system_clock::now());
+		    // string t = ctime(&end_time);
+		    // t = t.substr(10,9);
+            // printf("thread %d enqueued %d at %s\n",id+1,enq,t.c_str() );
+
+		}
+		else{
+			int ret = bs_q.dequeue();
+            printf("thread %d dequeued %d\n", id+1, ret);
+            /*****************************************************
+             comment above line and uncomment below lines
+             to get time stamps. This could give outofrange error
+             which we could not rectify, but it does not have anything
+             to do with the working of the queue
+            ******************************************************/
+            // time_t end_time = chrono::system_clock::to_time_t(chrono::system_clock::now());
+            // string t = ctime(&end_time);
+            // t = t.substr(10,9);
+            // printf("thread %d dequeued %d at %s\n", id+1, ret, t.c_str());
+		}
+	}
+}
+
+
+int main(){
+
+    cout<<"Enter number of threads (n) : "<<endl;
+    cin>>n;
+    cout<<"Enter number of operations for each thread (k): "<<endl;
+    cin>>k;
     thread test_threads[n];
+    auto EnterTime = chrono::steady_clock::now();
     for(int i=0;i<n;i++)
     {
-        test_threads[i] = thread(test,i);    
+        test_threads[i] = thread(test_random,i);    
     }
-    
+	auto ExitTime = chrono::steady_clock::now();
+	auto total_time = chrono::duration_cast<chrono::nanoseconds>(ExitTime-EnterTime);
+    auto time =(total_time.count());
+
     for(int i=0;i<n;i++)
     {
         test_threads[i].join();
     }
+
+
+    /*********************************************************
+    comment lines 313-325 and uncomment the below lines 333-344
+    to test the average time per operation of test_alternate
+    **********************************************************/
+    
+    // auto EnterTime = chrono::steady_clock::now();
+    // for(int i=0;i<n;i++)
+    // {
+    //     test_threads[i] = thread(test_alternate,i);    
+    // }
+	// auto ExitTime = chrono::steady_clock::now();
+	// auto total_time = chrono::duration_cast<chrono::nanoseconds>(ExitTime-EnterTime);
+    // auto time =(total_time.count());
+    // for(int i=0;i<n;i++)
+    // {
+    //     test_threads[i].join();
+    // }
+    
+    cout<<"Total time taken : "<<time/1000<<" microseconds"<<endl;
+    time/=(n*k);
+    cout<<"Average time taken for each operation : "<<time<<" nanoseconds"<<endl;
+
     return 0;
 }
