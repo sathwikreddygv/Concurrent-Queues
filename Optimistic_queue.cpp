382a2ce66d82de8f03d116eea7608f2e7024ff4e
sathwikreddygv <49616121+sathwikreddygv@users.noreply.github.com>
2020-12-23 11:36:59 +0530

    Add files via upload

--- Optimistic_queue.cpp
+++ Optimistic_queue.cpp
@@ -3,14 +3,22 @@
 #include <unistd.h>
 #include <ctime> //for times
 #include <chrono>    // for timers 
-#include<thread>    //for threads
-#include<random> //for exponential distribution
+#include <thread>    //for threads
+#include <random> //for exponential distribution
+#include <atomic>
+
+using namespace std;
+class pointer_t;
+class node_t;
+class queue_t;
+
+void fixList(queue_t *q,pointer_t tail,pointer_t head);
 
 class pointer_t{
     public:
     node_t* ptr;
     int tag;
-    pointer_t()
+    pointer_t() noexcept
     {
         ptr = NULL;
         tag = 0;
@@ -28,11 +36,105 @@ class node_t{
     }
 };
 
+bool comparePointer(pointer_t a,pointer_t b)
+{
+    if((a.ptr != b.ptr)||(a.tag != b.tag))
+    {
+        return false;
+    }
+    return true;
+}
+
+class queue_t{
+    public:
+    atomic<pointer_t> tail;
+    atomic<pointer_t> head;
+    
+    // queue_t() noexcept
+    // {
+
+    // }
+
+    queue_t(queue_t *q)
+    {
+        node_t* nd = new node_t();
+        
+        nd->next.ptr = NULL;
+        nd->next.tag = 0;
+        pointer_t temp;
+        temp.ptr=nd;
+        temp.tag=0;
+        (q->tail).store(temp);
+       // (q->tail).store().tag = 0;
+        (q->head).store(temp);
+       // (q->head).store().tag = 0;
+    }
+
+    void enqueue(queue_t* q, int val)
+    {
+        pointer_t check_tail;
+        node_t* nd = new node_t();
+        nd->value = val;
+        while(1)
+        {
+            check_tail = (q->tail).load();
+            nd->next.ptr = check_tail.ptr;
+            nd->next.tag = check_tail.tag+1;
+            pointer_t temp;
+            temp.ptr = nd;
+            temp.tag = check_tail.tag+1;
+            if((q->tail).compare_exchange_strong(check_tail, temp)) //
+            {
+                (check_tail.ptr)->prev.ptr = nd;
+                (check_tail.ptr)->prev.tag = check_tail.tag+1;
+                break;
+            }
+        }
+    }
+
+    int dequeue(queue_t* q)
+    {
+        pointer_t check_tail,check_head,firstNodePrev;
+        int val;
+        while(1)
+        {
+            check_head = (q->head).load();
+            check_tail = (q->tail).load();
+            firstNodePrev = (check_head.ptr)->prev;
+            if(comparePointer(check_head, (q->head).load()))
+            {
+                if(!comparePointer(check_tail,check_head))
+                {
+                    if(firstNodePrev.tag!=check_head.tag)
+                    {
+                        fixList(q,check_tail,check_head);
+                        continue;
+                    }
+                    val = (firstNodePrev.ptr)->value;
+                    pointer_t temp;
+                    temp.ptr = firstNodePrev.ptr;
+                    temp.tag = check_head.tag+1;
+                    if((q->head).compare_exchange_strong(check_head, temp)) //
+                    {
+                        delete check_head.ptr ;//????
+                        return val;
+                    }
+                }
+            }
+            else
+            {
+                return INT_MIN;
+            }
+        }
+    }
+
+};
+
 void fixList(queue_t *q,pointer_t tail,pointer_t head)
 {
     pointer_t curNode, curNodeNext;
     curNode = tail;
-    while((head == q->head)&&(curNode != head))
+    while((comparePointer(head,(q->head.load())))&& (!comparePointer(curNode, head)))
     {
         curNodeNext = (curNode.ptr)->next;
         (curNode.ptr)->prev.ptr = curNode.ptr; 
@@ -42,74 +144,6 @@ void fixList(queue_t *q,pointer_t tail,pointer_t head)
     }
 }
 
-class queue_t{
-    public:
-    pointer_t tail;
-    pointer_t head;
-
-    queue_t(queue_t *q)
-    {
-        node_t* nd = new node_t();
-        
-        nd->next.ptr = NULL;
-        nd->next.tag = 0;
-        q->tail.ptr = nd;
-        q->tail.tag = 0;
-        q->head.ptr = nd;
-        q->head.tag = 0;
-    }
-
-    void enqueue(queue_t* q, int val)
-    {
-        pointer_t tail;
-        node_t* nd = new node_t();
-        nd->value = val;
-        while(1)
-        {
-            tail = q->tail;
-            nd->next.ptr = tail.ptr;
-            nd->next.tag = tail.tag+1;
-            if() //
-            {
-                (tail.ptr)->prev.ptr = nd;
-                (tail.ptr)->prev.tag = tail.tag+1;
-                break;
-            }
-        }
-    }
-
-    int dequeue(queue_t* q)
-    {
-        pointer_t tail,head,firstNodePrev;
-        int val;
-        while(1)
-        {
-            head = q->head;
-            tail = q->tail;
-            firstNodePrev = (head.ptr)->prev;
-            if(head == q->head)
-            {
-                if(tail!=head)
-                {
-                    if(firstNodePrev.tag!=head.tag)
-                    {
-                        fixList(q,tail,head)
-                        continue;
-                    }
-                    val = (firstNodePrev)->value;
-                    if(CAS) //
-                    {
-                        delete head.ptr //????
-                        return val;
-                    }
-                }
-            }
-            else
-            {
-                return NULL;
-            }
-        }
-    }
-
-};
-
+int main(){
+    return 0;
+}
